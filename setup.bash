e8f0db7fcded0b8fbd2ec76b5b84efde589bb271
Eiji Mori <chronolavos0124@gmail.com>
2018-05-01 10:06:53 +0000

    Define crontab

--- setup.bash
+++ setup.bash
@@ -1,10 +1,7 @@
 #!/bin/bash -xve
 
 exec 2> /tmp/setup.log
-<<<<<<< HEAD
 
-=======
->>>>>>> 620e2705aff78c42dad4c60306228756b4e0ed5d
 cd /home/ubuntu/RaspberryPiMouse/src/drivers/
 /sbin/insmod rtmouse.ko
 
